eabd81df8f9b97b8650f86732a676d4013a52787
Bertrand CHERONNET <>
2025-04-15 13:08:58 +0200

    Feature: Moved message to constants

--- checker.py
+++ checker.py
@@ -1,15 +1,14 @@
+INVALID_LENGTH_MESSAGE = "Please enter a strict 5 letters word."
+
 def is_valid_length(guess):
-	if len(guess) != 5:
-		print("Please enter a strict 5 letter's word.")
-		return False
-	return True
+    if len(guess) != 5:
+        print(INVALID_LENGTH_MESSAGE)
+        return False
+    return True
 
 def is_exist(guess, word_lib):
-	return True
+    return True
 
 def check_word(word, guess, nb_try, word_lib):
-	if not is_valid_length(guess) or not is_exist(guess, word_lib):
-		return False
-
-
-
+    if not is_valid_length(guess) or not is_exist(guess, word_lib):
+        return False
