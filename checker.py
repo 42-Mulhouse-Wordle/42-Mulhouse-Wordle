4515e764d4a26e059bf3408ba2dd25f490312ca2
allan hans <allan.hans68350@gmail.com>
2025-04-15 13:34:32 +0200

    Chore: pending exist

--- checker.py
+++ checker.py
@@ -5,11 +5,14 @@ def is_valid_length(guess):
 	return True
 
 def is_exist(guess, word_lib):
+	if guess not in word_lib:
+		print("This word doesn't exist in the dictionary.")
+		return False
 	return True
 
 def check_word(word, guess, nb_try, word_lib):
 	if not is_valid_length(guess) or not is_exist(guess, word_lib):
 		return False
-
+	
 
 
