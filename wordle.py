4518a2a1941aa6f8f2528838c80fa7e66cb4be43
Bertrand CHERONNET <>
2025-04-16 20:29:16 +0200

    Chore: Refactor console display

--- wordle.py
+++ wordle.py
@@ -1,30 +1,11 @@
 import random
-from rich.theme import Theme
-from rich.console import Console
-from rich.table import Table
 from checker import check_word
-from dictionary_handler import fill_wordLib
+from dictionary_handler import fill_wordLib, print_screen
 
 GUESS_PROMPT = "input: "
-TITLE = ":writing_hand: :brain: :bulb: [info]Wordle[/] :bulb: :brain: :writing_hand:\n"
-HEADER = "Guesses"
 LOSS_MESSAGE = "You lost, the word was: "
 EOF_CATCH = "End of File detected, exiting...\n"
 NEW_LINE = "\n"
-
-def setup_console():
-	custom_theme = Theme(
-	    {"info": "cyan", "placed": "bold bright_green", "correct": "bold bright_yellow", "wrong": "bright_black"}
-	)
-	console = Console(theme=custom_theme)
-	console.clear()
-	console.rule(TITLE)
-	table = Table()
-	table.add_column(HEADER, style="wrong", justify="center")
-	for i in range(6):
-		table.add_row("- - - - -")
-	console.print(table, justify="center")
-	return console
 
 def run_game(word, word_lib):
 	while word_lib.guessing and word_lib.tries_left > 0:
@@ -43,7 +24,7 @@ def main():
 	word_lib = fill_wordLib()
 	if word_lib:
 		word = random.choice(word_lib.words)
-		setup_console()
+		print_screen()
 		run_game(word, word_lib)
 
 main()
